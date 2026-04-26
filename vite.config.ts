f7df497ca1c0038dc7e6ae1f4f05d6424647bca6
mailtoraja69 <mailtoraja69@gmail.com>
2026-04-26 13:32:36 -0400

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Use '/rathnavel-pon/' while on GitHub Pages subdomain
-// Change to '/' after connecting your custom domain
-export default defineConfig({
-  plugins: [react()],
-  base: '/rathnavel-pon/',
-})
