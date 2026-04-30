be079a79a6500b4adf2e9ea7fdc42ffdd24ad226
mailtoraja69 <mailtoraja69@gmail.com>
2026-04-30 11:04:36 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -455,7 +455,6 @@ const advisoryClients = [
   { name: 'Prajai Times',                    url: 'http://prajaitimes.in',                                                           abbr: 'PT'  },
   { name: 'BESTOW',                          url: 'https://www.instagram.com/bestow.cbe/',                                           abbr: 'BW'  },
   { name: 'Elite Construction & Architects', url: 'https://www.facebook.com/people/Elite-Construction-And-Architects/61571073511891/', abbr: 'EC'  }
-  { name: 'IRAAH Photography', url: 'https://www.instagram.com/iraahphotography/', abbr: 'EC'  }
 ]
 function Advisory() {
   const { ref, v } = useReveal()
@@ -520,8 +519,6 @@ const socials = [
   { name: 'Pinterest',   handle: 'ponrathnavel', url: 'https://pin.it/34KNJeLko',
     icon: <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor"><path d="M12 0C5.373 0 0 5.373 0 12c0 5.084 3.163 9.426 7.627 11.174-.105-.949-.2-2.405.042-3.441.218-.937 1.407-5.965 1.407-5.965s-.359-.719-.359-1.782c0-1.668.967-2.914 2.171-2.914 1.023 0 1.518.769 1.518 1.69 0 1.029-.655 2.568-.994 3.995-.283 1.194.599 2.169 1.777 2.169 2.133 0 3.772-2.249 3.772-5.495 0-2.873-2.064-4.882-5.012-4.882-3.414 0-5.418 2.561-5.418 5.207 0 1.031.397 2.138.893 2.738a.36.36 0 0 1 .083.345l-.333 1.36c-.053.22-.174.267-.402.161-1.499-.698-2.436-2.889-2.436-4.649 0-3.785 2.75-7.262 7.929-7.262 4.163 0 7.398 2.967 7.398 6.931 0 4.136-2.607 7.464-6.227 7.464-1.216 0-2.359-.632-2.75-1.378l-.748 2.853c-.271 1.043-1.002 2.35-1.492 3.146C9.57 23.812 10.763 24 12 24c6.627 0 12-5.373 12-12S18.627 0 12 0z"/></svg>,
     desc: 'Photos of people who inspire him', color: '#E60023' },
-  { name: 'Facebook',    handle: 'reavan',      url: 'https://www.facebook.com/reavan',
-    icon: <Facebook size={20}/>, desc: 'Community updates and engagement', color: '#1877F2' },
 ]
 function Connect() {
   const { ref, v } = useReveal()
